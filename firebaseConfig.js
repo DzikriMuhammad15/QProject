a42fb04002450f23c314c8071b955ff9b1427c50
DzikriMuhammad15 <mrpdzikri@gmail.com>
2026-05-16 10:26:25 +0700

    add debuging firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,23 @@
 // firebaseConfig.js
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+console.log('FIREBASE_SERVICE_ACCOUNT exists:', !!process.env.FIREBASE_SERVICE_ACCOUNT);
+console.log('DATABASE_URL exists:', !!process.env.DATABASE_URL);
+console.log('FIREBASE_SERVICE_ACCOUNT first 30 chars:', (process.env.FIREBASE_SERVICE_ACCOUNT || '').substring(0, 30));
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+    throw new Error('Environment variable FIREBASE_SERVICE_ACCOUNT is not set.');
+}
+if (!process.env.DATABASE_URL) {
+    throw new Error('Environment variable DATABASE_URL is not set.');
+}
+
+let serviceAccount;
+try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} catch (e) {
+    throw new Error('Failed to parse FIREBASE_SERVICE_ACCOUNT: ' + e.message);
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
